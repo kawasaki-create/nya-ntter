28e816d78ca15a8a6965a44ea5a75221f6102cec
kawasaki <r.kawasaki.biz@gmail.com>
2022-10-07 07:17:49 +0900

    DB接続できた（ローカル）

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -19,9 +19,32 @@ return Socialite::driver('twitter')->redirect();
 * @return \Illuminate\Http\Response
 */
 public function handleProviderCallback()
-{
-$user = Socialite::driver('twitter')->user();
-Log::info('Twitterから取得しました。', ['user' => $user]);
-// $user->token;
-}
+     {
+        try {
+            $twitterUser = Socialite::driver('twitter')->user();
+        } catch (Exception $e) {
+            return redirect('auth/twitter');
+        }
+         if(User::where('email', $twitterUser->getEmail())->exists()){
+            //ツイッターで作成されたユーザーならそのままパスする
+            $user = User::where('email', $twitterUser->getEmail())->first();
+            if(!$user->twitter){
+                dd("すでに同じメールアドレスが登録されています。");
+            }
+         }else{
+            $user = new User();
+            //ユーザーに必要な情報
+            $user->name = $twitterUser->getName();
+            $user->email = $twitterUser->getEmail();
+            $user->password = md5(Str::uuid());
+            $user->profile_photo_path = $twitterUser->getAvatar();
+            $user->twitter = true;
+            $user->nickname = $twitterUser->getNickname();
+            $user->save();
+            
+         }
+         Log::info('Twitterから取得しました。', ['user' => $twitterUser]);
+         Auth::login($user);
+         return redirect('/dashboard');
+     }
 }
